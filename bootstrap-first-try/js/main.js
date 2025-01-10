595a26a79eb3bb5d40ec8d46ee8f4e1be9efb727
sara khaled <sarahkhaled22003@gmail.com>
2025-01-10 16:23:26 +0200

    first time bootstrap

---
+++ bootstrap-first-try/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+//!______________________typed________________________//
+  var typed = new Typed("#element", {
+  strings: ["Trendy Threads for Trendsetters.", "Slay Every Day in Style"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+//!_________________AOS__________________//
+
+AOS.init();
+
+//!_________________AOS__________________//
